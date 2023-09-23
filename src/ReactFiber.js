5c3557d185ecac9a408ed08856e2f66a0efe2c99
Minouccc <343723899@qq.com>
2023-09-23 19:14:33 +0800

    feat: fiber任务执行

--- src/ReactFiber.js
+++ src/ReactFiber.js
@@ -1,28 +1,26 @@
-import {
-  ClassComponent,
-  Fragment,
-  FunctionComponent,
-  HostComponent,
-  HostText,
-} from "./ReactWorkTags";
-import { isFn, isStr, isUndefined, Placement } from "./utils";
+import { HostComponent } from "./ReactWorkTags";
+import { isFn, isStr } from "./utils";
 
 export function createFiber(vnode, returnFiber) {
   const fiber = {
     // 类型
     type: vnode.type,
+
     key: vnode.key,
     // 属性
     props: vnode.props,
-    // 不同类型的组件， stateNode也不同
+
+    // 不同类型的组件, stateNode不同
     // 原生标签 dom节点
     // class 实例
     stateNode: null,
 
     // 第一个子fiber
     child: null,
+
     // 下一个兄弟节点
     sibling: null,
+
     return: returnFiber,
 
     flags: Placement,
@@ -34,17 +32,11 @@ export function createFiber(vnode, returnFiber) {
   const { type } = vnode;
 
   if (isStr(type)) {
+    // 原生标签
     fiber.tag = HostComponent;
   } else if (isFn(type)) {
     // todo 函数以及类组件
-    fiber.tag = type.prototype.isReactComponent
-      ? ClassComponent
-      : FunctionComponent;
-  } else if (isUndefined(type)) {
-    fiber.tag = HostText;
-    fiber.props = { children: vnode };
-  } else {
-    fiber.tag = Fragment;
+    fiber.tag = FunctionComponent;
   }
 
   return fiber;
---
+++ src/ReactWorkLoop.js
@@ -0,0 +1,58 @@
+import { Fragment } from "react";
+import {
+  updateClassComponent,
+  updateFragmentComponent,
+  updateFunctionComponent,
+  updateHostComponent,
+  updateHostTextComponent,
+} from "./ReactReconciler";
+import {
+  ClassComponent,
+  FunctionComponent,
+  HostComponent,
+  HostText,
+} from "./ReactWorkTags";
+
+let wip = null; // work in progress 当前正在工作中的fiber
+
+function performUnitOfWork() {
+  const { tag } = wip;
+
+  // todo 1.更新当前组件
+  switch (tag) {
+    case HostComponent:
+      updateHostComponent(wip);
+      break;
+    case FunctionComponent:
+      updateFunctionComponent(wip);
+      break;
+    case ClassComponent:
+      updateClassComponent(wip);
+      break;
+    case Fragment:
+      updateFragmentComponent(wip);
+      break;
+    case HostText:
+      updateHostTextComponent(wip);
+    default:
+      break;
+  }
+  // todo 2.下一个更新谁 深度优先遍历
+
+  if (wip.child) {
+    wip = wip.child;
+    return;
+  }
+
+  let next = wip;
+
+  while (next) {
+    if (wip.sibling) {
+      wip = next.sibling;
+      return;
+    }
+    next = next.return;
+  }
+
+  wip = null;
+}
